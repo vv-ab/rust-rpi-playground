287f2905fcb04f957e231de4a742d6caa67ffc60
Vivian <bergerv.tin22@student.dhbw-heidenheim.de>
2023-04-20 17:17:12 +0200

    Added sending of structs over uart.

--- hello_uart/src/main.rs
+++ hello_uart/src/main.rs
@@ -1,17 +1,24 @@
 // uart_blocking_read.rs - Blocks while waiting for incoming serial data.
 
 use std::error::Error;
-use std::mem::forget;
+use std::mem::size_of;
 use std::time::Duration;
 
 use rppal::uart::{Parity, Uart};
 
+#[derive(serde::Serialize, serde::Deserialize, Debug, Eq, PartialEq)]
+struct Data {
+    id: u32,
+    //message: &'a str,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut uart = Uart::new(9600, Parity::None, 8, 1)?;
 
-    uart.set_read_mode(1, Duration::from_secs(1))?;
+    uart.set_read_mode(size_of::<Data>() as u8, Duration::from_secs(1))?;
 
-    let mut buffer = [0u8; 1024];
+    let mut buffer = [0u8; size_of::<Data>()];
+
     loop {
         let bytes_received = match uart.read(&mut buffer) {
             Ok(bytes) => {
@@ -28,9 +35,18 @@ fn main() -> Result<(), Box<dyn Error>> {
         if bytes_received > 0 {
             let answer = buffer[0].wrapping_add(1);
 
+            match postcard::from_bytes::<Data>(&buffer) {
+                Ok(data) => {
+                    println!("Successfully deserialized bytes into data: {data:?}");
+                }
+                Err(err) => {
+                    println!("failed to deserialize bytes into data: {err:?}");
+                }
+            };
+
             println!("Received message: {}, Answering with: {}", buffer[0], answer);
 
-            match uart.write(&[answer]) {
+            match uart.write(&[answer, 113u8]) {
                 Ok(bytes) => {
                     println!("Successfully sent {bytes} bytes");
                 }
