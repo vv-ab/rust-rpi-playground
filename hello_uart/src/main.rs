0ca5d6c28db47def9ba43a0a06a9d8b40b32bfe8
Vivian <bergerv.tin22@student.dhbw-heidenheim.de>
2023-04-20 14:54:16 +0200

    Implemented UART RX.

--- hello_uart/src/main.rs
+++ hello_uart/src/main.rs
@@ -1,5 +1,33 @@
+// uart_blocking_read.rs - Blocks while waiting for incoming serial data.
 
-#[entry]
-fn main() -> ! {
-    loop {}
+use std::error::Error;
+use std::time::Duration;
+
+use rppal::uart::{Parity, Uart};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut uart = Uart::new(9600, Parity::None, 8, 1)?;
+
+    uart.set_read_mode(1, Duration::from_secs(1))?;
+
+    let mut buffer = [0u8; 1024];
+    loop {
+
+        let bytes_received = match uart.read(&mut buffer) {
+            Ok(bytes) => {
+                bytes
+            }
+            Err(a) => {
+                println!("error while reading from uart: {:?}", a);
+                continue
+            }
+        };
+
+        println!("received {} bytes", bytes_received);
+
+        if bytes_received > 0 {
+            let message = String::from_utf8(Vec::from(buffer))?;
+            println!("Received message: {}", message);
+        }
+    }
 }
